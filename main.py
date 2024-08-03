66bc10266d792e5698ab2f8b7d34a0c4d598a737
vstovbunskyi <vaderxv@gmail.com>
2024-08-03 22:44:48 +0200

    Basic gameplay loop;

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import random
 
 # TODO:
-""" Tags
-"default_model": {
-  "provider": "anthropic",
-  "model": "claude-3-5-sonnet-20240620"
-}
-"""
+# Tags:
+    # #look #item etc
+# Knowledge chains (acyclic directed graph):
+    # GoindAroundTheWorld -> Got80Days -> ItsABet
 
 dict_characters = {
     "Cassandra": "#tsundere #foxgirl",
@@ -17,10 +15,21 @@ dict_characters = {
     "Randy Hightower": "#radiantdawn"
 }
 
-print("The night beckons~ OwO")
+user_action = ""
+
 #killer_character = random.choice(list(dict_characters.keys()))
 #killer_character_tags = random.choice(list(dict_characters.values()))
 killer_character, killer_character_tags = random.choice([*dict_characters.items()])
 print("The killer is " + killer_character + " (" + killer_character_tags + ") !")
 
-input()
+while(user_action != "quit"):
+    print("The night beckons~ OwO\n What do you do?")
+    print (""" * (talk) with [Cassandra / Mildred / MrWhiskers]
+ * (search) for clues""")
+
+    user_action = input()
+
+    if (user_action == "talk"):
+        print("You talked")
+    elif (user_action == "search"):
+        print("You've found an important clue!")
